34bbc3d707f8aee2f52785f18cba97e22018347d
Ashish0014 <138017791+Ashish0014@users.noreply.github.com>
2025-05-30 16:23:22 +0530

    Add src/ File: pages/index.tsx

---
+++ src/ File: pages/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+
+/**
+ * Index page component.
+ */
+const IndexPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Welcome to Habibi!</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
