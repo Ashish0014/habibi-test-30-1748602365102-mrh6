352547d29848d4624d8d7c4b2c58bcf686682ea4
Ashish0014 <138017791+Ashish0014@users.noreply.github.com>
2025-05-30 16:23:24 +0530

    Add src/ File: pages/dashboard.tsx

---
+++ src/ File: pages/dashboard.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+
+/**
+ * Dashboard page component.
+ */
+const DashboardPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
