a0b1b89691fcc09db31675718c541667947021d9
Ashish0014 <138017791+Ashish0014@users.noreply.github.com>
2025-05-30 16:23:25 +0530

    Add src/ File: pages/profile.tsx

---
+++ src/ File: pages/profile.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+
+/**
+ * Profile page component.
+ */
+const ProfilePage: NextPage = () => {
+  return (
+    <div>
+      <h1>User Profile</h1>
+    </div>
+  );
+};
+
+export default ProfilePage;
